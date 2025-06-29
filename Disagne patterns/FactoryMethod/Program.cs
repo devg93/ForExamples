04d53e8040acc6c1392826dbe95bd820b245a368
devg93 <gtugushi67@gmail.com>
2025-06-29 10:38:57 +0400

    init abstractFactory

---
+++ Disagne patterns/FactoryMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿public interface IMethodFactory
+{
+    void Print(string key);
+}
+
+
+public class Word : IMethodFactory
+{
+    public void Print(string key1)
+    {
+        Console.WriteLine($"word print this key ->> {key1} ");
+    }
+}
+
+public class Pdf : IMethodFactory
+{
+    public void Print(string key2)
+    {
+        Console.WriteLine($"Pdf print this key ->> {key2}");
+    }
+}
+
+//=================================================================//
+public abstract class Creator
+{
+    public abstract IMethodFactory CreateAnimal();
+}
+
+public class WordCretor : Creator
+{
+    public override IMethodFactory CreateAnimal()
+    => new Word();
+}
+
+public class PdfCretor : Creator
+{
+    public override IMethodFactory CreateAnimal()
+       => new Pdf();
+}
+
+//========================================================================
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Creator creator = new WordCretor();
+        creator.CreateAnimal().Print("word");
+
+        Creator creator2 = new PdfCretor();
+        creator2.CreateAnimal().Print("Pdf");
+
+        
+    }
+}
